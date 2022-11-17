d5dcb83ea4a500458840b13dfc103d7344813c97
MSameer192 <sameer192.official@gmail.com>
2022-11-17 23:55:11 +0500

    WeatherAPIUpdated

--- app.js
+++ app.js
@@ -1,19 +1,30 @@
 const { response } = require('express');
 const express = require('express')
 const https = require('node:https');
-
+const bodyParser = require('body-parser');
 
 const app = express();
 
-app.get('/', function(req, res){
 
-    const url = 'https://api.openweathermap.org/data/2.5/weather?q=karachi&appid=c43ace7dd4a46c1323c94cc483f92b18&units=metric' 
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/index.html')
+
+});
 
 
-    https.get(url, function(response){
+app.post('/', function (req, res) {
+
+
+    const query = req.body.cityName;
+    const key = 'c43ace7dd4a46c1323c94cc483f92b18'
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${query}&appid=${key}&units=metric`
+
+
+    https.get(url, function (response) {
         console.log(response.statusCode);
-
-        response.on('data', function(data){
+        response.on('data', function (data) {
             const weatherData = JSON.parse(data)
             const temp = weatherData.main.temp
             console.log(temp);
@@ -21,21 +32,25 @@ app.get('/', function(req, res){
             const description = weatherData.weather[0].description
             console.log(description);
 
-            res.send(`
-            
-            <p> The weather is currently ${description} </p>
-            <h1> The temperature of ${weatherData.name} is ${temp} celcius </h1>  
-            `) 
+            const icon = weatherData.weather[0].icon
+            console.log(icon);
 
-            
+            const imgURL = `http://openweathermap.org/img/wn/${icon}@2x.png`
+
+            res.write(`<h1> The temperature of ${weatherData.name} is ${temp} degree celcius </h1>`)
+            res.write(`<h3> The weather is currently ${description} </h3>`)
+
+            res.write(`<img src=${imgURL} alt="" />`)
+            res.send()
+
         })
     })
 
-    
 
-})
+});
 
-    
-app.listen(3000, function(){
+
+
+app.listen(3000, function () {
     console.log('Server is running');
 })
